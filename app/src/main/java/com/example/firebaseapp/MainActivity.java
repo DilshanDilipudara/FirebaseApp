cb43b49e59a21ec8ee91fbc6576d3f0e7f44d573
DilshanDilipudara <gunawardhanaudara@gmail.com>
2019-05-15 12:22:36 +0530

    push the name in firebase

--- app/src/main/java/com/example/firebaseapp/MainActivity.java
+++ app/src/main/java/com/example/firebaseapp/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.firebaseapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.firebase.client.Firebase;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private Button msendData;
+    private Firebase mRef;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Firebase.setAndroidContext(this);
+
+        mRef = new Firebase("https://fir-app-cdebb.firebaseio.com/");
+
+        msendData = (Button) findViewById(R.id.senddata);
+
+        msendData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Firebase mRefChild = mRef.child("Name");
+                mRefChild.setValue("Dilshan");
+            }
+        });
     }
 }
